0fce4c34819f841cb07bb0155e9428addc1538e5
PavelAd <adap2@mail.ru>
2022-12-28 17:09:29 +0400

    изменил для работы в контейнерах

--- etl/config/config.py
+++ etl/config/config.py
@@ -5,11 +5,11 @@ class Base(BaseSettings):
     db_name: str = 'movies_database'
     db_user: str = 'app'
     db_password: str = '123qwe'
-    db_path: str = 'db.sqlite'
-    host: str = '127.0.0.1'
+    # db_path: str = 'db.sqlite'
+    host: str = 'db'
     port: int = 5432
     # redis_name = 'localhost'
-    es_host: str = 'localhost'
+    es_host: str = 'elastic'
     es_port: int = 9200
     es_scheme: str = 'http'
 
