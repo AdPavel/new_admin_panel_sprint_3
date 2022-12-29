a74ddd162bb69905c6a41701547e8b378524ed97
PavelAd <adap2@mail.ru>
2022-12-29 13:22:26 +0400

    добавил настройки для таймеров, имя файла хранилища состояния

--- etl/config/config.py
+++ etl/config/config.py
@@ -2,16 +2,28 @@ from pydantic import BaseSettings
 
 
 class Base(BaseSettings):
+    # Postgres section
     db_name: str = 'movies_database'
     db_user: str = 'app'
     db_password: str = '123qwe'
-    # db_path: str = 'db.sqlite'
-    host: str = 'db'
     port: int = 5432
-    # redis_name = 'localhost'
-    es_host: str = 'elastic'
+    host: str = 'db'
+    # Для локального тестирования
+    # host: str = '127.0.0.1'
+
+    # Elastic section
     es_port: int = 9200
     es_scheme: str = 'http'
+    es_host: str = 'elastic'
+    # Для локального тестирования
+    # es_host: str = 'localhost'
+
+    # Backoff section
+    border_time: int = 30
+
+    # ETL section
+    iteration_pause: int = 30
+    storage_file: str = 'state_store.json'
 
     class Config:
         case_sensitive = False
